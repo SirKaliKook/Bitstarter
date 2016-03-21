a10db4ce176b1b780b1e0d74b2388daab5333dbf
SirKaliKook <try123100@gmail.com>
2016-03-21 23:56:12 +0000

    Updated index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express')
 var app = express()
+var fs = require('fs')
+
+var infile = "index.html";
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+  response.send(new Buffer(fs.readFiileSync(infile),"utf-8"))
 })
 
 app.listen(app.get('port'), function() {
