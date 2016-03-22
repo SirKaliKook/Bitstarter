2d56b9dcfe25530824c9717d17fcdbdf121b491d
SirKaliKook <try123100@gmail.com>
2016-03-22 00:02:15 +0000

    This is not update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.sendf(s.readFiileSync(infile))
+  response.send(fs.readFiileSync(infile))
 })
 
 app.listen(app.get('port'), function() {
