190785f15885f60575ed10da5afb44dba64fa310
SirKaliKook <try123100@gmail.com>
2016-03-26 02:27:06 +0000

    Changed to 8080

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var fs = require('fs')
 var infile = "index.html";
 var myBuf = fs.readFileSync(infile);
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
